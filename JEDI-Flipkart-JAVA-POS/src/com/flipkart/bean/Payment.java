61a01bc0af964f9d3b63f0cb238646a81965bbde
PARTH CHANDAN <75495326+ParthChandanPC@users.noreply.github.com>
2024-01-24 22:17:34 +0530

    Update Payment.java

--- JEDI-Flipkart-JAVA-POS/src/com/flipkart/bean/Payment.java
+++ JEDI-Flipkart-JAVA-POS/src/com/flipkart/bean/Payment.java
@@ -1,4 +1,32 @@
 package com.flipkart.bean;
 
 public class Payment {
+
+  public String creditCardNumber;
+	public String creditCardExpiry;
+
+	public String creditCardPIN;
+	public String getCreditCardPIN() {
+		return creditCardPIN;
+	}
+
+	public void setCreditCardPIN(String creditCardPIN) {
+		this.creditCardPIN = creditCardPIN;
+	}
+
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public String getCreditCardExpiry() {
+		return creditCardExpiry;
+	}
+
+	public void setCreditCardExpiry(String creditCardExpiry) {
+		this.creditCardExpiry = creditCardExpiry;
+	}
 }
